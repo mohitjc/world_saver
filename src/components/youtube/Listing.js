3276353f9e6026a7c1d37bde6d1d0c34f2ea5e22
mohitjc <mohitk@jcsoftwaresolution.com>
2021-02-19 16:05:34 +0530

    action

--- src/components/youtube/Listing.js
+++ src/components/youtube/Listing.js
@@ -90,7 +90,7 @@ const Listing = ({
                     <th>Description</th>
                     <th>Link</th>
                     {/* <th>Status</th> */}
-                    <th>Action</th>
+                    <th style={{minWidth:'250px'}}>Action</th>
                   </tr>
                   {items &&
                     items.map((item, index) => (

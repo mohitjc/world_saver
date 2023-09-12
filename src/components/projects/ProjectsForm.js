631c6b9be9a56151a7c8928737221e4f545370fd
Mohit Kumar <mkjcsoftwaresolution@gmail.com>
2023-09-12 18:27:32 +0530

    fg

--- src/components/projects/ProjectsForm.js
+++ src/components/projects/ProjectsForm.js
@@ -7,9 +7,7 @@ import Yup, {
   number as yupNumber
 } from 'yup';
 import swal from 'sweetalert';
-import { withRouter } from 'react-router-dom';
 
-import { isNull } from 'lodash';
 import {
   skillAdd,
   skillUpdate,

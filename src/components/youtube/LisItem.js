71698a7608daa4237cdc1f1bf35f192a97765123
Amit <amitk@jcsoftwaresolution.com>
2023-10-16 16:56:53 +0530

    changes

--- src/components/youtube/LisItem.js
+++ src/components/youtube/LisItem.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import swal from 'sweetalert';
 import dayjs from 'dayjs';
 import { items } from '../../store/actions/youtubeActions';
+import { trimEnd } from 'lodash';
 
 const ListItem = ({
   item,
@@ -79,17 +80,19 @@ const ListItem = ({
     <tr>
       <td>{index + page * count - (count - 1)}</td>
       <td>{item && item.title ? item.title : '___'}</td>
-      <td><span className=''>{item && item.description ? item.description : '___'}</span></td>
+      <td>{item && item.description ? item.description : '___'}</td>
       <td>{item && item.url ? item.url : '___'}</td>
     
       <td>
-        <div className='d-flex align-items-center'>
+        <div className='d-flex ' style={{height:'12vh'}}>
 
         
         <button
-          type="button" 
+          type="button"
+          style={{width:'50px',height:'7vh'}}
           className="btn btn-icon btn-primary mr-2 mt-2"
           onClick={() => {
+           
             handAddFormToggle(false);
             handleFormVisibilty();
             getId(item && item.id);
@@ -98,7 +101,8 @@ const ListItem = ({
           <i className="far fa-edit" />
         </button>
         <button
-          type="button" 
+          type="button"
+          style={{width:'50px',height:'7vh'}}
           className="btn btn-icon btn-danger mr-2 mt-2"
           onClick={handleDelete}
         >
@@ -107,15 +111,26 @@ const ListItem = ({
 
         <button
           type="button"
-          style={{width:'100px'}}
-          className="btn btn-icon btn-secondary mt-2 mr-2 text-center"
+          style={{width:'98px',height:'7vh'}}
+          className="btn btn-icon btn-secondary m-2 text-center"
           onClick={()=>handleArchive(item && item.id ,item && item.isArchive)}
         >
           {item && item.isArchive?'Un-Archive':'Archive'}
         </button>
 
-        {item && item.isFeatured?<a className="badge badge-success text-white">Featrued</a>:<button
-          type="button" 
+        {item && item.isFeatured?<button
+          type="button"
+          // style={{height:'7vh'}}
+          className=" btn btn-icon badge badge-success text-white mt-2"
+          style={{height:'7vh',borderRadius:'5px',textAlign:'center',marginTop:'6px',width:'80px'}}
+          
+        >
+          Featured
+        </button>
+        // <a className="badge badge-success text-white d-flex"   style={{height:'7vh',borderRadius:'5px',textAlign:'center',marginTop:'6px'}}>Featured</a>
+        :<button
+          type="button"
+          style={{height:'7vh'}}
           className="btn btn-icon btn-secondary mt-2"
           onClick={()=>handleFeature(item && item.id)}
         >

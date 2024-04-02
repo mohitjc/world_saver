a880ca878ea742d460f1972b236e37738b05be41
JC25 <vikashrajput@jcsoftwaresolution.com>
2024-04-02 18:38:05 +0530

    changes

--- src/components/user/UserListItem.js
+++ src/components/user/UserListItem.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import swal from 'sweetalert';
-import ApiClient from '../apiClient';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import swal from "sweetalert";
+import ApiClient from "../apiClient";
+import Loading from "../global/Loader";
+// import { toast } from "react-toastify";
 
 const UserListItem = ({
   item,
@@ -15,20 +17,21 @@ const UserListItem = ({
   page,
   resetStatus,
   getAllUser,
-  count
+  count,
 }) => {
-  console.log(page,"pagepage");
+  console.log(page, "pagepage");
+  const [form, setform] = useState({});
   const handleDelete = () => {
-    console.log(item, 'itemitemitem');
+    console.log(item, "itemitemitem");
 
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     swal({
-      title: 'Are you sure?',
-      text: 'you want to delete the user!',
-      icon: 'warning',
+      title: "Are you sure?",
+      text: "you want to delete the user!",
+      icon: "warning",
       buttons: true,
-      dangerMode: true
-    }).then(willDelete => {
+      dangerMode: true,
+    }).then((willDelete) => {
       if (willDelete) {
         deleteUser(item && item.id, token);
       } else {
@@ -37,29 +40,46 @@ const UserListItem = ({
     });
   };
 
-  const handleStatus = status => {
-    const token = localStorage.getItem('token');
+  const HandleCHangeStatus = (e) => {
+    e.preventDefault();
+    // Loading(TRUE)
+    ApiClient.put("/changestatus/user", form).then((res) => {});
+  };
+  const HandleUpdateStatus = (status) => {
+    let payload = {
+      id: item?.id,
+      reason: "",
+      status: "active",
+    };
+    // Loading(TRUE)
+    ApiClient.put("/changestatus/user", payload).then((res) => {
+      // toast.success(res?.data?.message);
+    });
+  };
+
+  const handleStatus = (status) => {
+    const token = localStorage.getItem("token");
     const obj = {
-      model: 'users',
+      model: "users",
       id: item && item.id,
-      status
+      status,
     };
     getStatus(status);
     changeStatus(obj, token);
   };
 
   // Guide button function
-  const guidestatus = (item) =>{
+  const guidestatus = (item) => {
     console.log(item);
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     const obj = {
-      isGuide: item.isGuide?false:true
+      isGuide: item.isGuide ? false : true,
     };
 
-    ApiClient.put('/user/'+item.id,obj).then(res=>{
-      getAllUser()
-    })
-  }
+    ApiClient.put("/user/" + item.id, obj).then((res) => {
+      getAllUser();
+    });
+  };
   // console.log('page * count ', page, count);
   // console.log('page * count ', page * count);
   // console.log('page * count ', page * count - (count - 1));
@@ -68,17 +88,17 @@ const UserListItem = ({
       <td>{index + page * count - (count - 1)}</td>
       <td>
         <Link to={`/user/${item && item.id}`}>
-          {item && item.fullName ? item.fullName : '___'}
+          {item && item.fullName ? item.fullName : "___"}
         </Link>
       </td>
       <td>{item && item.email}</td>
-      <td>{item && item.mobile ? item.mobile : '___'}</td>
+      <td>{item && item.mobile ? item.mobile : "___"}</td>
       <td>
-        {item && item.status === 'deactive' ? (
+        {item && item.status === "deactive" ? (
           <button
             type="button"
             className="badge badge-warning"
-            onClick={() => handleStatus('active')}
+            onClick={() => HandleUpdateStatus("active")}
           >
             Deactive
           </button>
@@ -86,7 +106,10 @@ const UserListItem = ({
           <button
             type="button"
             className="badge badge-success"
-            onClick={() => handleStatus('deactive')}
+            onClick={() => {
+              setform({ ...form, id: item?.id, status: "deactive" });
+              document.getElementById("OpenStatusModal").click();
+            }}
           >
             Active
           </button>
@@ -94,36 +117,114 @@ const UserListItem = ({
       </td>
       {/* Guide Button */}
       <td>
-      
-        {item.isGuide==true?
-        <button type="button" className="badge badge-success" onClick={() => guidestatus(item)}>Unguide</button>
-        :
-        <button type="button" className="badge badge-warning" onClick={() => guidestatus(item)}>Guide</button>
-        }  
-            
+        {item.isGuide == true ? (
+          <button
+            type="button"
+            className="badge badge-success"
+            onClick={() => guidestatus(item)}
+          >
+            Unguide
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="badge badge-warning"
+            onClick={() => guidestatus(item)}
+          >
+            Guide
+          </button>
+        )}
       </td>
+      <div
+        // style={{
+        //   zIndex: 100000,
+        // }}
+        class="modal fade"
+        id="exampleModalCenter"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">
+                Reson to Deactive
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form onSubmit={HandleCHangeStatus}>
+                <div>
+                  <label>Reason</label>
+                </div>
+                <textarea
+                  onChange={(e) => {
+                    setform({ ...form, reason: e.target.value });
+                  }}
+                  name=""
+                  id=""
+                  cols="40"
+                  rows="10"
+                ></textarea>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="submit" ty class="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       <td>
-      <div className='d-flex'>
-        <button
-          type="button"
-          className="btn btn-icon btn-primary "
-          onClick={() => {
-            handAddFormToggle(false);
-            handleFormVisibilty();
-            getUserId(item && item.id);
-          }}
-        >
-          <i className="far fa-edit" />
-        </button>
-        <button
-          type="button"
-          className="btn  btn-icon btn-danger ml-2"
-          onClick={handleDelete}
-        >
-          <i className="fas fa-trash" />
-        </button>
+        <div className="d-flex">
+          <button
+            type="button"
+            className="btn btn-icon btn-primary "
+            onClick={() => {
+              handAddFormToggle(false);
+              handleFormVisibilty();
+              getUserId(item && item.id);
+            }}
+          >
+            <i className="far fa-edit" />
+          </button>
+          <button
+            type="button"
+            className="btn  btn-icon btn-danger ml-2"
+            onClick={handleDelete}
+          >
+            <i className="fas fa-trash" />
+          </button>
         </div>
       </td>
+
+      <button
+        style={{ display: "none" }}
+        type="button"
+        id="OpenStatusModal"
+        class="btn btn-primary"
+        data-toggle="modal"
+        data-target="#exampleModalCenter"
+      >
+        Launch demo modal
+      </button>
     </tr>
   );
 };
